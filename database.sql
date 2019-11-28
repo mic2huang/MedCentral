cdbb3afb5302190b40d3002cfc8779ed8aae171f
Adam Clayman <adam@clayman.org>
2019-11-27 21:21:03 -0500

    Centralized user log-in details in User table

--- database.sql
+++ database.sql
@@ -8,46 +8,55 @@ DROP TABLE IF EXISTS medical_result;
 DROP TABLE IF EXISTS patient;
 DROP TABLE IF EXISTS hcp;
 DROP TABLE IF EXISTS researcher;
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE patient (
+CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(30) NOT NULL,
     pword VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE patient (
+    user_id INT NOT NULL,
     research_opt_in BOOLEAN NOT NULL DEFAULT FALSE,
     family_data_share BOOLEAN NOT NULL DEFAULT FALSE,
     deceased BOOLEAN NOT NULL DEFAULT FALSE,
     genetic_mother INT DEFAULT NULL,
     genetic_father INT DEFAULT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (genetic_mother) REFERENCES patient (id),
-    FOREIGN KEY (genetic_father) REFERENCES patient (id)
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (genetic_mother) REFERENCES patient (user_id),
+    FOREIGN KEY (genetic_father) REFERENCES patient (user_id),
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE hcp (
-    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
     username VARCHAR(30) NOT NULL,
     pword VARCHAR(30) NOT NULL,
     heirarchy_level INT NOT NULL DEFAULT 1,
     specialization VARCHAR(50) DEFAULT NULL,
     organization VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE patient_hcp (
     patient_id INT NOT NULL,
     hcp_id INT NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patient (id),
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (user_id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (user_id),
     PRIMARY KEY (patient_id, hcp_id)
 );
 
 CREATE TABLE researcher (
-    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
     username VARCHAR(30) NOT NULL,
     pword VARCHAR(30) NOT NULL,
     specialization VARCHAR(50) DEFAULT NULL,
     organization VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE genetic_result (
@@ -94,8 +103,8 @@ CREATE TABLE genetic_report (
     hcp_id INT NOT NULL,
     genetic_result_id INT DEFAULT NULL,
     FOREIGN KEY (genetic_result_id) REFERENCES genetic_result (id),
-    FOREIGN KEY (patient_id) REFERENCES patient (id),
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (user_id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (user_id),
     PRIMARY KEY (id)
 );
 
@@ -110,8 +119,8 @@ CREATE TABLE lab_report (
     hcp_id INT NOT NULL,
     lab_result_id INT DEFAULT NULL,
     FOREIGN KEY (lab_result_id) REFERENCES lab_result (id),
-    FOREIGN KEY (patient_id) REFERENCES patient (id),
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (user_id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (user_id),
     PRIMARY KEY (id)
 );
 
@@ -126,7 +135,7 @@ CREATE TABLE medical_report (
     hcp_id INT NOT NULL,
     medical_result_id INT DEFAULT NULL,
     FOREIGN KEY (medical_result_id) REFERENCES medical_result (id),
-    FOREIGN KEY (patient_id) REFERENCES patient (id),
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (user_id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (user_id),
     PRIMARY KEY (id)
 );
