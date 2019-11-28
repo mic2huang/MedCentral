9f02ead408c3cc2fb0f780cc232b85e295f39a6d
Adam Clayman <adam@clayman.org>
2019-11-27 21:46:42 -0500

    Added basic dummy data to database.sql

--- database.sql
+++ database.sql
@@ -32,8 +32,6 @@ CREATE TABLE patient (
 
 CREATE TABLE hcp (
     user_id INT NOT NULL,
-    username VARCHAR(30) NOT NULL,
-    pword VARCHAR(30) NOT NULL,
     heirarchy_level INT NOT NULL DEFAULT 1,
     specialization VARCHAR(50) DEFAULT NULL,
     organization VARCHAR(50) NOT NULL,
@@ -51,8 +49,6 @@ CREATE TABLE patient_hcp (
 
 CREATE TABLE researcher (
     user_id INT NOT NULL,
-    username VARCHAR(30) NOT NULL,
-    pword VARCHAR(30) NOT NULL,
     specialization VARCHAR(50) DEFAULT NULL,
     organization VARCHAR(50) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id),
@@ -139,3 +135,15 @@ CREATE TABLE medical_report (
     FOREIGN KEY (hcp_id) REFERENCES hcp (user_id),
     PRIMARY KEY (id)
 );
+
+INSERT INTO user VALUES (1, 'claymana', 123), (2, 'schlickr', 123), (3, 'cousinsr', 123), (4, 'tungh', 123), (5, 'huynhthu', 123);
+INSERT INTO patient VALUES (2, TRUE, TRUE, TRUE, NULL, NULL), (3, TRUE, TRUE, TRUE, NULL, NULL), (1, TRUE, TRUE, TRUE, 2, 3);
+INSERT INTO hcp VALUES (4, 1, 'Nephrology', 'Hillside Hospital');
+INSERT INTO patient_hcp VALUES (1, 4);
+INSERT INTO researcher VALUES (5, 'Healthcare Economics', 'Oregon State University');
+INSERT INTO genetic_result VALUES (1, 'Genetic B-12 Processing Deficiency', TRUE, TRUE, TRUE, 'B-12 Processing Deficiency is a fairly self-explanatory genetic disorder.', '2019-11-27');
+INSERT INTO lab_result VALUES (1, 'Lab Vitamin D Deficiency', TRUE, TRUE, TRUE, 'Vitamin D Deficiency is a common lab result in the Northern hemisphere.', '2019-11-28');
+INSERT INTO medical_result VALUES (1, 'Left femur hairline fracture', TRUE, TRUE, TRUE, 'Patient has broken her left femur in a hairline fracture.', '2019-11-29');
+INSERT INTO genetic_report VALUES (1, TRUE, TRUE, TRUE, 'Here is my report on Genetic B-12 Processing Deficiency', '2019-11-27', 1, 4, 1);
+INSERT INTO lab_report VALUES (1, TRUE, TRUE, TRUE, 'Here is my report on the lab result of a Vitamin D deficiency.', '2019-11-28', 1, 4, 1);
+INSERT INTO medical_report VALUES (1, TRUE, TRUE, TRUE, 'The patient suffered a hairline fracture of her left femur.', '2019-11-29', 2, 4, 1);
