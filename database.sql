dd51293e0e2818aa57ae3a9bdb13081992a2e859
Adam Clayman <adam@clayman.org>
2019-11-27 21:09:16 -0500

    Edited database.sql to hold the proper order of drop table commands

--- database.sql
+++ database.sql
@@ -1,4 +1,13 @@
+DROP TABLE IF EXISTS patient_hcp;
+DROP TABLE IF EXISTS genetic_report;
+DROP TABLE IF EXISTS lab_report;
+DROP TABLE IF EXISTS medical_report;
+DROP TABLE IF EXISTS genetic_result;
+DROP TABLE IF EXISTS lab_result;
+DROP TABLE IF EXISTS medical_result;
 DROP TABLE IF EXISTS patient;
+DROP TABLE IF EXISTS hcp;
+DROP TABLE IF EXISTS researcher;
 
 CREATE TABLE patient (
     id INT NOT NULL AUTO_INCREMENT,
@@ -10,11 +19,9 @@ CREATE TABLE patient (
     genetic_mother INT DEFAULT NULL,
     genetic_father INT DEFAULT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (genetic_mother) REFERENCES patient (id) ON DELETE SET NULL,
-    FOREIGN KEY (genetic_father) REFERENCES patient (id) ON DELETE SET NULL
+    FOREIGN KEY (genetic_mother) REFERENCES patient (id),
+    FOREIGN KEY (genetic_father) REFERENCES patient (id)
 );
-
-DROP TABLE IF EXISTS hcp;
 
 CREATE TABLE hcp (
     id INT NOT NULL AUTO_INCREMENT,
@@ -26,17 +33,13 @@ CREATE TABLE hcp (
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS patient_hcp;
-
 CREATE TABLE patient_hcp (
     patient_id INT NOT NULL,
     hcp_id INT NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patient (id) ON DELETE SET NULL,
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id) ON DELETE SET NULL,
+    FOREIGN KEY (patient_id) REFERENCES patient (id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
     PRIMARY KEY (patient_id, hcp_id)
 );
-
-DROP TABLE IF EXISTS researcher;
 
 CREATE TABLE researcher (
     id INT NOT NULL AUTO_INCREMENT,
@@ -46,62 +49,6 @@ CREATE TABLE researcher (
     organization VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
-
-DROP TABLE IF EXISTS genetic_report;
-
-CREATE TABLE genetic_report (
-    id INT NOT NULL AUTO_INCREMENT,
-    researcher_access BOOLEAN NOT NULL DEFAULT FALSE,
-    family_access BOOLEAN NOT NULL DEFAULT FALSE,
-    ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
-    notes TEXT,
-    creation_date DATE,
-    patient_id INT NOT NULL,
-    hcp_id INT NOT NULL,
-    genetic_result_id INT DEFAULT NULL,
-    FOREIGN KEY (genetic_result_id) REFERENCES genetic_result (id) ON DELETE SET NULL,
-    FOREIGN KEY (patient_id) REFERENCES patient (id) ON DELETE SET NULL,
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id) ON DELETE SET NULL,
-    PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS lab_report;
-
-CREATE TABLE lab_report (
-    id INT NOT NULL AUTO_INCREMENT,
-    researcher_access BOOLEAN NOT NULL DEFAULT FALSE,
-    family_access BOOLEAN NOT NULL DEFAULT FALSE,
-    ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
-    notes TEXT,
-    creation_date DATE,
-    patient_id INT NOT NULL,
-    hcp_id INT NOT NULL,
-    lab_result_id INT DEFAULT NULL,
-    FOREIGN KEY (lab_result_id) REFERENCES lab_result (id) ON DELETE SET NULL,
-    FOREIGN KEY (patient_id) REFERENCES patient (id) ON DELETE SET NULL,
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id) ON DELETE SET NULL,
-    PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS medical_report;
-
-CREATE TABLE medical_report (
-    id INT NOT NULL AUTO_INCREMENT,
-    researcher_access BOOLEAN NOT NULL DEFAULT FALSE,
-    family_access BOOLEAN NOT NULL DEFAULT FALSE,
-    ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
-    notes TEXT,
-    creation_date DATE,
-    patient_id INT NOT NULL,
-    hcp_id INT NOT NULL,
-    medical_result_id INT DEFAULT NULL,
-    FOREIGN KEY (medical_result_id) REFERENCES medical_result (id) ON DELETE SET NULL,
-    FOREIGN KEY (patient_id) REFERENCES patient (id) ON DELETE SET NULL,
-    FOREIGN KEY (hcp_id) REFERENCES hcp (id) ON DELETE SET NULL,
-    PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS genetic_result;
 
 CREATE TABLE genetic_result (
     id INT NOT NULL AUTO_INCREMENT,
@@ -114,8 +61,6 @@ CREATE TABLE genetic_result (
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS lab_result;
-
 CREATE TABLE lab_result (
     id INT NOT NULL AUTO_INCREMENT,
     result VARCHAR(255) NOT NULL,
@@ -127,8 +72,6 @@ CREATE TABLE lab_result (
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS medical_result;
-
 CREATE TABLE medical_result (
     id INT NOT NULL AUTO_INCREMENT,
     result VARCHAR(255) NOT NULL,
@@ -137,5 +80,53 @@ CREATE TABLE medical_result (
     ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
     notes TEXT,
     creation_date DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE genetic_report (
+    id INT NOT NULL AUTO_INCREMENT,
+    researcher_access BOOLEAN NOT NULL DEFAULT FALSE,
+    family_access BOOLEAN NOT NULL DEFAULT FALSE,
+    ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
+    notes TEXT,
+    creation_date DATE,
+    patient_id INT NOT NULL,
+    hcp_id INT NOT NULL,
+    genetic_result_id INT DEFAULT NULL,
+    FOREIGN KEY (genetic_result_id) REFERENCES genetic_result (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE lab_report (
+    id INT NOT NULL AUTO_INCREMENT,
+    researcher_access BOOLEAN NOT NULL DEFAULT FALSE,
+    family_access BOOLEAN NOT NULL DEFAULT FALSE,
+    ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
+    notes TEXT,
+    creation_date DATE,
+    patient_id INT NOT NULL,
+    hcp_id INT NOT NULL,
+    lab_result_id INT DEFAULT NULL,
+    FOREIGN KEY (lab_result_id) REFERENCES lab_result (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE medical_report (
+    id INT NOT NULL AUTO_INCREMENT,
+    researcher_access BOOLEAN NOT NULL DEFAULT FALSE,
+    family_access BOOLEAN NOT NULL DEFAULT FALSE,
+    ancestor_access BOOLEAN NOT NULL DEFAULT FALSE,
+    notes TEXT,
+    creation_date DATE,
+    patient_id INT NOT NULL,
+    hcp_id INT NOT NULL,
+    medical_result_id INT DEFAULT NULL,
+    FOREIGN KEY (medical_result_id) REFERENCES medical_result (id),
+    FOREIGN KEY (patient_id) REFERENCES patient (id),
+    FOREIGN KEY (hcp_id) REFERENCES hcp (id),
     PRIMARY KEY (id)
 );
